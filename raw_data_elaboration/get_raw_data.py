ec307a7fae7c7d5a8bb56fa74657d6622f5829ed
Mirko Lukovic <mirko.lukovic@wsl.ch>
2025-04-23 14:05:02 +0200

    raw data tools updated

--- raw_data_elaboration/get_raw_data.py
+++ raw_data_elaboration/get_raw_data.py
@@ -39,7 +39,7 @@ def server_data_dendrometer(database, data_path, credentials_path):
     """Connects to the TreeNet server and downloads the entire metadata table and all the non-empty time series data.
         The metadata is a pandas dataframe and the timeseries data is stored as a list of pandas dataframes."""
     
-    write_text(data_path+"/screen.log", "Getting " + database + " ...")
+    write_text(data_path+"/screen_" + database + ".log", "Getting " + database + " ...")
     with open(credentials_path, 'r') as f:
         credentials = yaml.full_load(f)
 
@@ -58,14 +58,15 @@ def server_data_dendrometer(database, data_path, credentials_path):
                               credentials.get('port'), 
                               credentials.get('dbname') )
     
-    query = "SELECT series_id,ts,value FROM " + database + " WHERE series_id=%s ORDER BY ts" 
+    # query = "SELECT series_id,ts,value FROM " + database + " WHERE series_id=%s ORDER BY ts" 
+    query = "SELECT * FROM " + database + " WHERE series_id=%s ORDER BY ts LIMIT 10" 
     
     for row in meta.iterrows():
         #if row[1].tree_species == '-999':
         #    print('series id: ' + str(row[1].series_id) + ' -> empty')
         # else:
 
-        if row[1].variable_name == 'tree stem radius change':  
+        if row[1].variable_name == "tree stem radius change":  
             # NOTE: makes sure that only dendrometer data is selected. there is also other sensor metadata stored in the same file.
             print('series id: ' + str(row[1].series_id))
             series_id = row[1].series_id
@@ -79,26 +80,27 @@ def server_data_dendrometer(database, data_path, credentials_path):
                                                       credentials.get('port'), 
                                                       credentials.get('dbname') )
             if len(temp) > 10:      
-                write_text(data_path+"/screen.log", 'series id: ' + str(series_id) + ' -> OK')
+                write_text(data_path+"/screen_" + database + ".log", 'series id: ' + str(series_id) + ' -> OK')
                 data[series_id] = temp
                 new_meta.append(row[1])
             else:
-                write_text(data_path+"/screen.log", 'series id: ' + str(series_id) + ' -> empty or less than 10 rows')
+                write_text(data_path+"/screen_" + database + ".log", 'series id: ' + str(series_id) + ' -> empty or less than 10 rows')
 
-    with open(data_path+"/"+database+"._dictionary.pkl", 'wb') as f:
+    with open(data_path+"/"+database+"_dictionary.pkl", 'wb') as f:
         pickle.dump(data, f)
 
     new_meta = pd.DataFrame(new_meta)
-    with open(data_path+"/metadata_dendrometer.pkl", 'wb') as f:
+    with open(data_path+"/metadata_" + database + ".pkl", 'wb') as f:
         pickle.dump(new_meta, f)
 
-    write_text(data_path+"/screen.log", "Done...")
+    write_text(data_path+"/screen_" + database + ".log", "Done...")
+
 
 def server_data_climate(database, data_path, credentials_path):
     """Connects to the TreeNet server and downloads the entire metadata table and all the non-empty time series data.
         The metadata is a pandas dataframe and the timeseries data is stored as a list of pandas dataframes."""
     
-    write_text(data_path+"/screen.log", "Getting " + database + " ...")
+    write_text(data_path+"/screen_" + database + ".log", "Getting " + database + " ...")
     with open(credentials_path, 'r') as f:
         credentials = yaml.full_load(f)
 
@@ -131,12 +133,81 @@ def server_data_climate(database, data_path, credentials_path):
                                                 credentials.get('port'), 
                                                 credentials.get('dbname')
                                                 )
-        write_text(data_path+"/screen.log", 'series id: ' + str(site_id) + ' -> OK')
+        write_text(data_path+"/screen_" + database + ".log", 'series id: ' + str(site_id) + ' -> OK')
 
-    with open(data_path+"/"+database+"_dictionary.pkl", 'wb') as f:
+    with open(data_path + "/" + database + "_dictionary.pkl", 'wb') as f:
         pickle.dump(data, f)
 
-    write_text(data_path+"/screen.log", "Done...")
+    write_text(data_path+"/screen_" + database + ".log", "Done...")
+
+
+def server_data(database, data_path, credentials_path):
+    """Connects to the TreeNet server and downloads the entire metadata table and all the non-empty time series data.
+        The metadata is a pandas dataframe and the timeseries data is stored as a list of pandas dataframes."""
+    
+    write_text(data_path+"/screen_" + database + ".log", "Getting " + database + " ...")
+    with open(credentials_path, 'r') as f:
+        credentials = yaml.full_load(f)
+
+    temperature = {}
+    humidity = {}
+    temperature_meta = []
+    humidity_meta = []
+    meta = server.get_metadata( credentials.get('user'), 
+                                credentials.get('password'), 
+                                credentials.get('host'), 
+                                credentials.get('port'), 
+                                credentials.get('dbname') )
+    
+    server.get_database_info( database,
+                              credentials.get('user'), 
+                              credentials.get('password'), 
+                              credentials.get('host'), 
+                              credentials.get('port'), 
+                              credentials.get('dbname') )
+    
+    query = "SELECT ts, series_id, value FROM " + database + " WHERE series_id=%s ORDER BY ts"  
+    
+    for row in meta.iterrows():
+        variable = row[1].variable_name
+        if variable == "air temperature" or variable == "relative humidity":  
+            # NOTE: makes sure that only dendrometer data is selected. there is also other sensor metadata stored in the same file.
+            print('series id: ' + str(row[1].series_id))
+            series_id = row[1].series_id
+            value = (series_id,)
+
+            temp = server.get_data_element(value, 
+                                                      query,
+                                                      credentials.get('user'), 
+                                                      credentials.get('password'), 
+                                                      credentials.get('host'), 
+                                                      credentials.get('port'), 
+                                                      credentials.get('dbname') )
+            if len(temp) > 10:      
+                write_text(data_path+"/screen_" + database + ".log", 'series id: ' + str(series_id) + ' -> OK')
+                if variable == "air temperature":
+                    temperature[series_id] = temp
+                    temperature_meta.append(row[1])
+                else:
+                    humidity[series_id] = temp
+                    humidity_meta.append(row[1])
+            else:
+                write_text(data_path+"/screen_" + database + ".log", 'series id: ' + str(series_id) + ' -> empty or less than 10 rows')
+
+    with open(data_path + "/" + database + "_temperature_dictionary.pkl", 'wb') as f:
+        pickle.dump(temperature, f)
+    with open(data_path + "/" + database + "_humidity_dictionary.pkl", 'wb') as f:
+        pickle.dump(humidity, f)
+
+    temperature_meta = pd.DataFrame(temperature_meta)
+    with open(data_path+"/metadata_" + database + "_temperature.pkl", 'wb') as f:
+        pickle.dump(temperature_meta, f)
+
+    humidity_meta = pd.DataFrame(humidity_meta)
+    with open(data_path+"/metadata_" + database + "_humidity.pkl", 'wb') as f:
+        pickle.dump(humidity_meta, f)
+
+    write_text(data_path+"/screen_" + database + ".log", "Done...")
 
 
 if __name__ == "__main__":
@@ -148,11 +219,11 @@ if __name__ == "__main__":
 
     if args.database == "metadata":
         server_metadata(args.path, args.credentials_path)
-    elif args.database == "dendrometer":
-        server_data_dendrometer("data_dendro_lm", args.path, args.credentials_path)
-    elif args.database == "climate":
-        server_data_climate("data_meteo_l2", args.path, args.credentials_path)
+    elif args.database == "data_dendro_lm" or args.database == "data_dendro_l2":
+        server_data_dendrometer(args.database, args.path, args.credentials_path)
+    elif args.database == "data_meteo_lm" or args.database == "data_meteo_l2":
+        server_data_climate(args.database, args.path, args.credentials_path)
     else:
-        print("Error! Incorrect choice of database.")
+        server_data(args.database, args.path, args.credentials_path)
 
     # TODO: create a function that puts all the channels together into a single multi-channel time series.
