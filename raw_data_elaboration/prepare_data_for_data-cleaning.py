f1c6b1c56bae53a0de478b5768239f9e5e1f915b
Mirko Lukovic <mirko.lukovic@wsl.ch>
2025-06-02 16:32:33 +0200

    1a file and prepare_data done

--- raw_data_elaboration/prepare_data_for_data-cleaning.py
+++ raw_data_elaboration/prepare_data_for_data-cleaning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import argparse
 import pickle
+import os
 
 import tools.data_processing_library as dpl
 import tools.data_organisation as do
@@ -16,6 +17,10 @@ if __name__ == "__main__":
     parser.add_argument("temperature_l1_path", type=str)
     parser.add_argument("humidity_l1_path", type=str)
 
+    parser.add_argument("clima_l2_path", type=str)
+    parser.add_argument("clima_lm_path", type=str)
+    parser.add_argument("cosmo_data_path", type=str)
+
     parser.add_argument("year_start", type=str)
     parser.add_argument("year_end", type=str)
 
@@ -26,8 +31,13 @@ if __name__ == "__main__":
     meta = dpl.load_dataframe(args.meta_path)
     meta_temperature_l1 = dpl.load_dataframe(args.meta_temperature_l1_path)
     meta_humidity_l1 = dpl.load_dataframe(args.meta_humidity_l1_path)
+    
     temperature_l1 = dpl.load_dataframe(args.temperature_l1_path)
     humidity_l1 = dpl.load_dataframe(args.humidity_l1_path)
+    clima_l2 = dpl.load_dataframe(args.clima_l2_path)
+    clima_lm = dpl.load_dataframe(args.clima_lm_path)
+    cosmo_data_path = dpl.load_dataframe(args.cosmo_data_path)
+
     dendro = dpl.load_dataframe(args.dendro_path)
     clima = dpl.load_dataframe(args.clima_path)
     
@@ -86,23 +96,59 @@ if __name__ == "__main__":
     ############################################################### E N D ########################################################################
 
     # 1. Put together the raw weather data from the TreeNet database
-
     
-    # 2. Put together the data product from COSMO
-    data_path = '/storage/lukovic/Data/FORWARDS/treenet/COSMO_FromCirrus/'
-    site_coordinates = do.get_site_coordinates(sites, meta)
-    clima_cosmo = do.get_site_cosmo_clima(site_coordinates, year_start, year_end, data_path)  # NOTE: dictionary (key = site_id, value = datarame [ts, temperature, relative humidity])
+    # NOTE: get sites with temperature sensors
+    site_groups = meta_temperature_l1.groupby('site_id')
+    temperature_sensors = {}
+    for i, row in site_groups:
+        temperature_sensors[i] = row.series_id
 
-    # 3. Merge together the TreeNet data and the COSMO data
+    # NOTE: get sites with humidity sensors
+    site_groups = meta_humidity_l1.groupby('site_id')
+    humidity_sensors = {}
+    for i, row in site_groups:
+        humidity_sensors[i] = row.series_id
 
-    input_df = {}
+    # NOTE: get sites that have all the necessary data
+    sites = list(set(temperature_sensors.keys()) & set(humidity_sensors.keys()) & set(clima_lm.keys()) & set(clima_l2.keys()))
+
+    site_coordinates = get_site_coordinates(sites, meta)
+    clima_cosmo = get_site_cosmo_clima(site_coordinates, 2017, 2017, cosmo_data_path)
+
+    weather_data = []
+    weather_data_identifiers = []
     for site in sites:
-        clima_input = pd.merge(clima_cosmo[site], clima_l1[site], on = "ts", how = "outer")
-        input_df[site] = clima_input[clima_input.cosmo_temp.first_valid_index():clima_input.cosmo_temp.last_valid_index()]  # NOTE: remove the head and tail where "value" is not defined
+        for temp_id in temperature_sensors[site]:
+            for humidity_id in humidity_sensors[site]:
+                df_temp_raw = temperature_l1[temp_id][['ts', 'value']]  # NOTE: raw temperature
+                df_temp_raw.columns = ['ts', 'temp_raw']
+                df_rh_raw = humidity_l1[humidity_id][['ts', 'value']]  # NOTE: raw relative humidity
+                df_rh_raw.columns = ['ts', 'rh_raw']
+                df_temp_processed = clima_l2[site][['ts', 'temp']]  # NOTE: curated temperature
+                df_temp_processed.columns = ['ts', 'temp_processed']
+                df_rh_processed = clima_lm[site][['ts', 'rh']]  # NOTE: curated relative humidity
+                df_rh_processed.columns = ['ts', 'rh_processed']
 
-    # 4. Prepare the ground truth data from TreeNet (the Lm weather data)
+                # NOTE: the ground truth data might have NaNs at the beginning and end.
+                df_temp_processed = df_temp_processed.dropna()  # NOTE: remove NaNs.
+                # NOTE: make sure that all the time stamps are present
+                start = df_temp_processed.ts.iloc[0]
+                end = df_temp_processed.ts.iloc[-1]
+                complete_times = {'ts': pd.date_range(start=start, end=end, freq='1h')}   # NOTE: alternative: "10Min". For more information see https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#timeseries-offset-aliases
+                df_temp_processed = pd.merge(df_temp_processed, pd.DataFrame(complete_times), on = "ts", how = "outer")
 
-    
+                # NOTE: First merge the input data
+                input_df = pd.merge(df_temp_raw, df_rh_raw, how='outer', on='ts')
+                # NOTE: Then merge the ground truth data
+                ground_truth_df = pd.merge(df_temp_processed, df_rh_processed, how='left', on='ts')
+                # NOTE: Then merge all the data into a single data frame
+                treenet_df = pd.merge(input_df, ground_truth_df, how='right', on='ts')
+                total_df = pd.merge(clima_cosmo[site], treenet_df, how='inner', on='ts')
+                
+                weather_data.append(total_df)
+                weather_data_identifiers.append([site, temp_id, humidity_id])  # NOTE: this is the corresponding list that contains the site and series IDs
 
-
-    
+    with open(args.output_folder + "weather_data.pkl", 'wb') as f:
+        pickle.dump(weather_data, f)
+    with open(args.output_folder + "weather_data_ids.pkl", 'wb') as f:
+        pickle.dump(weather_data_identifiers, f)
--- task_scripts/1a_prepare_data_for_data-cleaning.sh
+++ task_scripts/1a_prepare_data_for_data-cleaning.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
-clima_l2="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_meteo_l2_dictionary.pkl"
-clima_lm="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_meteo_lm_dictionary.pkl"
-temperature_l0="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_all_l0_temperature_dictionary.pkl"
-temperature_l1="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_all_l1_temperature_dictionary.pkl"
-humidity_l0="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_all_l0_humidity_dictionary.pkl"
-humidity_l1="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_all_l1_humidity_dictionary.pkl"
+########################################## D E S C R I P T I O N ###################################################
+# 
+# Evaluation process:
+# -------------------
+# The input consists of a single temperature signal and relative humidity signal from the same site. 
+# Therefore, the input must come in pairs. Besides the signals, the coordinates of the sensors or site is required. 
+# It is assumed that both sensors have the same coordinates. These coordinates are then used to 
+# determine the temperature and relative humidity from outside products.
+#
+# Training process:
+# -----------------
+# During the training, the raw temperature and relative humidity signals are combinatorily paired together and then
+# the outside climate data is added for those coordinates. For example, if a certain site has 3 temperature sensors
+# [t1, t2, t3] and 2 relative humidity signals [h1, h2], then the data from this site will look like the following 
+# (ot = outside temperature, oh = outside relative humidity):
+# s1 -> [t1, h1, ot, oh]
+# s2 -> [t1, h2, ot, oh]
+# s3 -> [t2, h1, ot, oh]
+# s4 -> [t2, h2, ot, oh]
+# s5 -> [t3, h1, ot, oh]
+# s6 -> [t3, h2, ot, oh]
+# Therefore, there will be 6 input points in this case. The important thing is that the input structure is always
+# constant and equal to 4 in this case. 
+# The output is a 2 channel time series, that contains the corrected and improved temperature and relative humidity
+# signal.
+####################################################################################################################
 
 meta="/storage/lukovic/Data/FORWARDS/treenet/server_data/metadata_all.pkl"
-meta_temperature_l0="/storage/lukovic/Data/FORWARDS/treenet/server_data/metadata_data_all_l0_temperature.pkl"
 meta_temperature_l1="/storage/lukovic/Data/FORWARDS/treenet/server_data/metadata_data_all_l1_temperature.pkl"
-meta_humidity_l0="/storage/lukovic/Data/FORWARDS/treenet/server_data/metadata_data_all_l0_humidity.pkl"
 meta_humidity_l1="/storage/lukovic/Data/FORWARDS/treenet/server_data/metadata_data_all_l1_humidity.pkl"
 
+temperature_l1="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_all_l1_temperature_dictionary.pkl"
+humidity_l1="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_all_l1_humidity_dictionary.pkl"
 
-out_folder="/home/lukovic/data/treenet/server_data/" 
+clima_l2="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_meteo_l2_dictionary.pkl"
+clima_lm="/storage/lukovic/Data/FORWARDS/treenet/server_data/data_meteo_lm_dictionary.pkl"
+cosmo_data_folder="/storage/lukovic/Data/FORWARDS/treenet/COSMO_FromCirrus/"
 
-python3 ~/codes/treenetai/raw_data_elaboration/prepare_data_for_data-cleaning.py $meta_path $dendro_path $clima_path $out_folder
+out_folder="/home/lukovic/data/treenet/server_data/processed"
 
-mv /home/lukovic/data/treenet/server_data/* /storage/lukovic/Data/FORWARDS/treenet/server_data/
+python3 ~/codes/treenetai/raw_data_elaboration/prepare_data_for_data-cleaning.py $meta_path $meta_temperature_l1 $meta_humidity_l1 $temperature_l1 $humidity_l1 $clima_l2 $clima_lm $cosmo_data_folder $out_folder
+
+mv /home/lukovic/data/treenet/server_data/processed* /storage/lukovic/Data/FORWARDS/treenet/server_data/processed
