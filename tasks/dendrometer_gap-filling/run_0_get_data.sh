8870040987270dfe42252f727a6dc8501ee77f60
Mirko Lukovic <mirko.lukovic@wsl.ch>
2025-07-11 15:05:36 +0200

    path changed

--- tasks/dendrometer_gap-filling/run_0_get_data.sh
+++ tasks/dendrometer_gap-filling/run_0_get_data.sh
@@ -7,11 +7,12 @@
 #                   "tree stem radius change", "air temperature", "relative humidity"
 ##############################################################################################################################################
 
-credentials_path=~/codes/treenetai/raw_data_elaboration/tools/config_server.yml
-path=~/data/treenet/server_data
+credentials_path=/home/lukovic/codes/treenetai/raw_data_elaboration/tools/config_server.yml
+path=/home/lukovic/data/treenet/server_data
 
 for database in metadata data_dendro_lm; do
-    python3 ~/codes/treenetai/raw_data_elaboration/get_raw_data.py $database $path $credentials_path
+    echo $database
+    python3 /home/lukovic/codes/treenetai/raw_data_elaboration/get_raw_data.py $database $path $credentials_path
     mv /home/lukovic/data/treenet/server_data/* /storage/lukovic/Data/FORWARDS/treenet/server_data
 done
 
