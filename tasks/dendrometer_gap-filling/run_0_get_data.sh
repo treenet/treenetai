2e89fbc73f3b2caf97bdd5d0808112e3c77f49c3
Mirko Lukovic <mirko.lukovic@wsl.ch>
2025-09-14 10:44:24 +0200

    run_0_get_data.sh changed

--- tasks/dendrometer_gap-filling/run_0_get_data.sh
+++ tasks/dendrometer_gap-filling/run_0_get_data.sh
@@ -7,12 +7,35 @@
 #                   "tree stem radius change", "air temperature", "relative humidity"
 ##############################################################################################################################################
 
+THRESHOLD=$((400 * 1024 * 1024))
 credentials_path=/home/lukovic/codes/treenetai/raw_data_elaboration/tools/config_server.yml
 path=/home/lukovic/data/treenet/server_data
 
+task() {
+    python3 /home/lukovic/codes/treenetai/raw_data_elaboration/get_raw_data.py $1 $2 $3 &
+    PID=$!
+    echo "${PID}"
+    while true; do
+        TOT_RAM_USAGE=$(free | awk '/Mem:/ {print $3}')
+        if [ "$TOT_RAM_USAGE" -gt "$THRESHOLD" ]; then
+            echo "RAM usage ($TOT_RAM_USAGE KB) exceeds the threshold ($THRESHOLD KB). Killing the process: ${PID}"
+            kill -9 "$PID"
+            exit 0
+        else
+            echo "Process with PID $PID, Total RAM usage is $TOT_RAM_USAGE KB."
+        fi
+        sleep 5
+        if ! ps -p "$PID" > /dev/null 2>&1; then
+            echo "Process with PID $PID is no longer running."
+            break
+        fi
+    done
+}
+
+
 for database in metadata data_dendro_l2; do
     echo $database
-    python3 /home/lukovic/codes/treenetai/raw_data_elaboration/get_raw_data.py $database $path $credentials_path
+    task $database $path $credentials_path
     mv /home/lukovic/data/treenet/server_data/* /storage/lukovic/Data/FORWARDS/treenet/server_data
 done
 
